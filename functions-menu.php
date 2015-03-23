58546bb7c48399090b0b124a3e38f3471f44f8a0
Willy Bahuaud <rouge13scompany@hotmail.com>
2015-03-23 18:55:55 +0100

    Fonctionne
    Fonctionne chez moi :-)

--- functions-menu.php
+++ functions-menu.php
@@ -1,49 +1,19 @@
-<?php
-
-function nav_menu_add_years( $items, $args )
-{
-	if ( 'annees' == $args->theme_location )
-	{
-		$today = getdate();
-
-		$year =$today['year'];
-
-		$items = "<li><a>" . $year . "</a></li>";
-
-
-		do {
-				$items .=  "<li><a>" . $year . "</a></li>";
-
-				$years = $years-1;
-
-				$args = array(
-						"date_query" => array(
-															"year" => $year
-														),
-					);
-
-				$the_query = new WP_Query( $args );
-
-			}
-
-		while (  $the_query->have_posts());
-	}
-	return $items;
+function nav_menu_add_years( $items, $args ) {
+    if ( 'annees' == $args->theme_location ) {
+        $today = getdate();
+        $year =$today['year'];
+        $items .= "<li><a>" . $year . "</a></li>";
+        do {
+                $items .=  "<li><a>" . $year . "</a></li>";
+                $year = $year-1;
+                $args = array(
+                        "date_query" => array(
+                            "year" => $year
+                        ),
+                    );
+                $the_query = new WP_Query( $args );
+        } while (  $the_query->have_posts() );
+    }
+    return $items;
 }
-
 add_filter( 'wp_nav_menu_items', 'nav_menu_add_years', 10, 2 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
